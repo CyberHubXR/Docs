9a8afb38e530f6310839a5723cc98d449cb9e3b6
WireWhiz <eli.knisley@spatialape.com>
2024-02-10 15:34:49 -0700

    set up docusaurus, swap out branding

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,7 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+export default function HomepageFeatures() {
+  return <h1>Hello World!</h1>
+}
