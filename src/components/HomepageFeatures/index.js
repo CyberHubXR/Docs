596f42f65b406594639ec801d6adba750915bfc0
WireWhiz <eli.knisley@spatialape.com>
2024-04-22 17:16:05 -0600

    Clean up homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,5 +3,16 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 export default function HomepageFeatures() {
-  return <h1>Hello World!</h1>
+  return <div className="homempage-links">
+      <div className="homepage-link-container">
+        <Heading as="h2">
+          <a href="/docs/category/foundry-core">Brane Core</a>
+        </Heading>
+      </div>
+      <div className="homepage-link-container">
+        <Heading as="h2">
+          <a href="/docs/category/foundry-core">Foundry</a>
+        </Heading>
+      </div>
+    </div>
 }
