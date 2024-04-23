c989b644d1bab701cddd62c0d6742d79f6b5b539
WireWhiz <eli.knisley@spatialape.com>
2024-04-23 17:50:36 -0600

    update site preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="The one place for all CyberHub docs">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
